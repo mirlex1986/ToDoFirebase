cb626a2a41b7e4a310dc2d26854edcd76c95d4b6
alex <mirlex1986@gmail.com>
2021-03-22 11:56:39 +0300

    add tasks view controller

--- ToDoFirebase/Controllers/LoginViewController.swift
+++ ToDoFirebase/Controllers/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewControllerViewController.swift
 //  ToDoFirebase
 //
 //  Created by Алексей on 22.03.2021.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ ToDoFirebase/Controllers/TasksViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TasksViewController.swift
+//  ToDoFirebase
+//
+//  Created by Алексей on 22.03.2021.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
